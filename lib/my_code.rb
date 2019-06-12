b72e70a1006dad4e26195f0051e76a4fbaa1b059
Pete Hanner <petehanner@gmail.com>
2019-06-12 18:01:55 -0400

    Wrote custom map and reduce functions

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,49 @@
-# Your Code Here
+def map(array)
+  # create a new array to store values
+  new_array = []
+  # make sure block is provided when calling method
+  if block_given?
+    # iterate over the array, do whatever's in the block to each item, and push the result into the new array
+    i = 0
+    while i < array.length
+      new_array << yield(array[i])
+      i += 1
+    end
+    # return new array
+    new_array
+  else
+    # error message
+    "Hey! You have to give a block."
+  end
+end
+
+def reduce(array, start_val = nil)
+  # make sure block is provided when calling method
+  if block_given?
+    # if start value given
+    # initialize return value with it
+    # start looping from array beginning
+    if start_val
+      rtn = start_val
+      i = 0
+      # if no start value given
+      # initialize return value with first array member
+      # start looping from next array member
+    else
+      rtn = array[0]
+      i = 1
+    end
+    # loop over array from given starting point
+    # pass current return value and current array member into block
+    # overwrite return value with result and move to next array member
+    while i < array.length
+      rtn = yield(rtn, array[i])
+      i += 1
+    end
+    # return value
+    rtn
+  else
+    # error message
+    "Hey! You have to give a block."
+  end
+end
