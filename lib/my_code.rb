745d912bd2c7e4b978ca0776534c9b8e5751870e
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 04:06:29 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,28 @@
 # Your Code Here
 
 def map(source_array)
-  new = [
-  
+  new = []
+  i = 0
+  while i < source_array.length do 
+    new.push(yield(source_array[i]))
+    i += 1
+  end
+  new
 end
+
+def reduce(source_array, starting_value = nil)
+  if starting_value
+    value = starting_value
+    i = 0
+  else 
+    value = source_array[0]
+    i = 1
+  end
+  while i < source_array.length
+    value = yield(value, source_array[i])
+    i += 1
+  end
+  value
+end
+
+
