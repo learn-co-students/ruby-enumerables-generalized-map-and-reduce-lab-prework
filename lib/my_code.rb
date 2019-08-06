c95bbc1edfd233d5f15209c7394660d71f2dfdf9
powell956 <powell956@gmail.com>
2019-08-06 16:30:01 -0400

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  array.each do |el|
+    new_array << yield(el)
+  end
+  new_array
+end
+
+def reduce(array, starting_value=nil)
+  if starting_value
+    reduction = starting_value
+    i = 0
+  else
+    reduction = array[0]
+    i = 1
+  end
+  while i < array.length
+    reduction = yield(reduction, array[i])
+    i += 1
+  end
+  reduction
+end
