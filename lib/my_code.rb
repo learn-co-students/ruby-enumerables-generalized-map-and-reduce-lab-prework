e090fc22fc81f1ba6bf78a431abc0d6ff2d157b7
Janu Sung <janu.m.sung@gmail.com>
2020-02-21 22:22:22 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
-# Your Code Here
+
+def map(array)
+  new_array = []
+  i = 0 
+  
+  array.length.times do 
+    new_array << yield(array[i])
+    i += 1 
+  end 
+  
+  new_array
+end 
+
+
+def reduce(array, starting_value = nil) 
+  if starting_value
+    result = starting_value
+    i = 0 
+  else 
+    result = array[0]
+    i = 1 
+  end 
+  
+  while i < array.length do 
+    result = yield(result, array[i])
+    i += 1 
+  end 
+  
+  result 
+end 
