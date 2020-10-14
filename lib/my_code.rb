c9361bcdc7ddbbfea4951435ccda59313e2e3b6b
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-10-14 00:28:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
-# Your Code Here
+require 'pry'
+
+def map(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array << yield(source_array[i])
+    i += 1 
+  end
+  return new_array
+end
