87b372da1695e2cf9e7c2b88ed33584daec302de
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-10-17 16:29:38 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,16 @@ def map(source_array)
   end
   return new_array
 end
+
+def reduce(source_array, starting_point = 0)
+  total = starting_point
+  i = 0 
+  if !source_array[i].is_a? Integer
+    total = source_array[i]
+  end
+  while i < source_array.length
+    total = yield(total, source_array[i])
+    i += 1
+  end
+  return total
+end
