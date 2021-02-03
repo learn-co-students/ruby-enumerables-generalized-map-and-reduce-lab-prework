4d46bf6dfe3d770d9d2b87875e83d839fc73f2c1
Jabair Khan <jabairk@gmail.com>
2021-02-03 00:05:47 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,12 +5,19 @@ def map(source_array)
   new_array.push(yield(source_array[index]))
   index += 1 
 end
- 
  new_array
 end
  #source_array.map{|n| n * -1}
 
-
+def reduce(source_array, starting_point)
+  new_value = []
+  index = 0 
+  while index < source_array.length
+  new_value.sum(yield(source_array[starting_point]))
+  index += 1 
+end
+new_value
+end
 
 
 
