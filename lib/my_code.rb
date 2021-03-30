5bbb16e91b90dbb8f5e8d8bb9687f1d17a20280a
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-30 20:32:14 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+def map(array)
+  index  = 0
+  mapped = []
+  
+  while index < array.length do
+    mapped << yield(array[index])
+    index   = index + 1
+  end
+  
+  mapped
+end
+
+def reduce(array, reduction = 0)
+  index  = 0
+  
+  while index < array.length do
+    if !reduction
+      break
+    end
+
+    reduction = yield(array[index], reduction)
+    index     = index + 1
+  end
+  
+  reduction
+end
